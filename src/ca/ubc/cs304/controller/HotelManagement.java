0bc31e1745b072086e6adbce21869e42f70177ee
elisaliuliu <eliisaliiu8@gmail.com>
2022-03-29 12:59:50 -0700

    i

---
+++ src/ca/ubc/cs304/controller/HotelManagement.java
@@ -0,0 +1,15 @@
+package ca.ubc.cs304.controller;
+
+import ca.ubc.cs304.database.DatabaseConnectionHandler;
+import ca.ubc.cs304.delegates.LoginWindowDelegate;
+import ca.ubc.cs304.delegates.TerminalTransactionsDelegate;
+import ca.ubc.cs304.model.BranchModel;
+import ca.ubc.cs304.ui.LoginWindow;
+import ca.ubc.cs304.ui.TerminalTransactions;
+
+public class HotelManagement {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
